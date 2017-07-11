3f7437abcbc0952a1aaa8564d25c5cbe9d1a16aa
Stuart Hoffman <stuhoffman@users.noreply.github.com>
2017-07-10 22:36:46 -0700

    First commit

---
+++ Weathered/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Weathered
+//
+//  Created by Stuart Hoffman on 7/10/17.
+//  Copyright © 2017 Stuart Hoffman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
