c73b81ee2f0db99bc6c5c6d599d1d394150e7565
Stuart Hoffman <stuhoffman@users.noreply.github.com>
2017-07-11 22:14:34 -0700

    Starting the UI

--- Weathered/WeatherVC.swift
+++ Weathered/WeatherVC.swift
@@ -16,6 +16,15 @@ class WeatherVC: NSViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear() {
+        //
+    }
+    
+    override func viewDidAppear() {
+        self.view.layer?.backgroundColor = CGColor(red: 0.29, green: 0.72, blue: 0.98, alpha: 1.00)
+        
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
